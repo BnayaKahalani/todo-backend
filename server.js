f33923ceaa5c945537956e3e50b9e94e142ffa54
Bnaya Kahalani <bnaya50@gmail.com>
2023-07-06 14:08:12 +0300

    add env for prod

--- server.js
+++ server.js
@@ -2,14 +2,24 @@ require('dotenv').config()
 const cors = require('cors')
 const express = require('express')
 const mongoose = require('mongoose')
+const path = require('path')
 const todoRoutes = require('./routes/todos')
 const userRoutes = require('./routes/user')
 
 const app = express()
 mongoose.set('strictQuery', true);
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.resolve(__dirname, 'public')))
+} else {
+  const corsOptions = {
+    origin: ['http://127.0.0.1:5173', 'http://127.0.0.1:8080', 'http://localhost:8080', 'http://127.0.0.1:3000', 'http://localhost:3000'],
+    credentials: true
+  }
+  app.use(cors(corsOptions))
+}
+
 app.use(express.json())
-app.use(cors())
 
 app.use((req, res, next) => {
   console.log(req.path, req.method, res)
