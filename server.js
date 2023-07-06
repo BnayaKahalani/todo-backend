b421c8cf8946f799b1b2daf489b1361ef543ec5b
Bnaya Kahalani <bnaya50@gmail.com>
2023-07-06 14:35:28 +0300

    deploy 1.1

--- server.js
+++ server.js
@@ -12,11 +12,11 @@ mongoose.set('strictQuery', true);
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.resolve(__dirname, 'public')))
 } else {
-  const corsOptions = {
-    origin: ['http://127.0.0.1:5173', 'http://127.0.0.1:8080', 'http://localhost:8080', 'http://127.0.0.1:3000', 'http://localhost:3000'],
-    credentials: true
-  }
-  app.use(cors(corsOptions))
+  // const corsOptions = {
+  //   origin: ['http://localhost:5174', 'http://127.0.0.1:5174', 'http://127.0.0.1:8080', 'http://localhost:8080', 'http://127.0.0.1:3000', 'http://localhost:3000'],
+  //   credentials: true
+  // }
+  app.use(cors())
 }
 
 app.use(express.json())
